2ebb6f7ca316203d74078f2ed4b4053f9e77cf1d
Isaac Bailey <isaac.bailey@smiledirectclub.com>
2020-04-17 12:59:24 -0500

    Pull the fizzbuzz calculation out of the loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,16 @@
-def fizz_buzz_generator(N):
-    for i in range(N):
-        value = ''
-        if i % 5 == 0:
-            value += 'fizz'
-        if i % 3 == 0:
-            value += 'buzz'
-        if i == 0 or value == '':
-            value = str(i)
-        yield value
+def calculate_value(number):
+    value = ''
+    if number % 5 == 0:
+        value += 'fizz'
+    if number % 3 == 0:
+        value += 'buzz'
+    if number == 0 or value == '':
+        value = str(number)
+    return value
+
+def fizz_buzz_generator(upper_limit):
+    for i in range(upper_limit):
+        yield calculate_value(i)
 
 for x in fizz_buzz_generator(101):
     print(x)
